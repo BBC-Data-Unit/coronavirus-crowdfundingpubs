b2bab65a0a63c67c231d6e76f708300bb30f0036
Paul Bradshaw <paulonhismobile@gmail.com>
2020-04-25 12:19:00 +0100

    Add files via upload

---
+++ coronacrowd.R
@@ -0,0 +1,71 @@
+pacman::p_load('dplyr', 'tidyr', 'gapminder',
+               'ggplot2',  'ggalt',
+               'forcats', 'R.utils', 'png', 
+               'grid', 'ggpubr', 'scales',
+               'bbplot2')
+
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+#save the column with the text in (using the column called "title" in the crowdfunder detail scrape csv ) as a .txt file
+
+#link to the data is https://drive.google.com/file/d/13Tyt9cIxMeA60_hYTdbIzIRjsCKLy0v7/view?usp=sharing, column E
+
+# This next bit opens up a file selector. Choose the text file
+text <- readLines(file.choose(""))
+
+docs <- Corpus(VectorSource(text))
+
+inspect(docs)
+
+
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+#docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+
+#get your list of words and frequency
+
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+
+
+#This is meant to do a wordcloud but it doesn't look that great.
+set.seed(1234)
+
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+write.csv(d, "covidallwords.csv")
+
+
